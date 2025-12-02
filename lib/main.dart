844ca15c596594e822eddf2978fc680902ab72fa
AbdulRahman <arahman443634@gmail.com>
2025-12-02 11:17:04 +0500

    The 2nd stuff
    its about Text styling and Background stuff with gradient..

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,30 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        //backgroundColor: Colors.amber,( to change whole app BG)      
+        //backgroundColor: Colors.amber,( to change whole app BG) we can add Gradient in it as well by putting  DecoratedBox
+        // inside the body: then add decoration: BoxDecoration( gradient: LinearGradient( colors: [.....]
         body: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors:[const Color.fromARGB(255, 8, 44, 252),const Color.fromARGB(255, 0, 19, 76)],
+              colors: [
+                const Color.fromARGB(255, 8, 44, 252),
+                const Color.fromARGB(255, 0, 19, 76),
+              ],
               begin: Alignment.topLeft,
-              end: Alignment.bottomRight)),
-          child: const Center
-          (child: Text("Bismillah")),
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 60,
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+              ),
+              "Allah ",
+            ),
+          ),
         ),
       ),
     ),
