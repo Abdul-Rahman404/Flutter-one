634125074f44c9cdc85b9bc431c80f2ae57f057b
AbdulRahman <arahman443634@gmail.com>
2025-12-15 20:34:00 +0500

    line 22

---
+++ centering the text in button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class answerButton extends StatelessWidget{
+const answerButton(this.answerText,{super.key,required this.onTap,});
+
+final String answerText;
+final void Function() onTap ; 
+
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor:Color.fromARGB(255, 20, 106, 176),
+        foregroundColor: Colors.white, //Text color inside Button
+        padding: EdgeInsets.symmetric(vertical: 10,horizontal: 40)
+
+       // shape:RoundedRectangleBorder(
+        //  borderRadius: BorderRadiusGeometry.circular(20),
+       // )  all this to add coustome rounded corners
+
+      ),
+      onPressed:onTap, 
+      child: Text(answerText, textAlign: TextAlign.center,) 
+            );
+  }
+}
